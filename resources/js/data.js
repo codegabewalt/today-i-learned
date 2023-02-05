3be3fb3f4a7a6e66d38994068fb02aa1d48a628e
codegabewalt <code.gabe.walt@gmail.com>
2023-02-05 12:45:12 +0000

    Worked on index and style

--- resources/js/data.js
+++ resources/js/data.js
@@ -2,7 +2,7 @@ const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
   { name: "science", color: "#16a34a" },
   { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
+  { name: "society", color: "ç" },
   { name: "entertainment", color: "#db2777" },
   { name: "health", color: "#14b8a6" },
   { name: "history", color: "#f97316" },
